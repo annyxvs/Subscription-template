413bae7168bbbb0c96610c7e7cff38242161d727
Anny Gabrielly <92173477+annyxvs@users.noreply.github.com>
2022-04-24 01:15:17 -0300

    Update global.ts

--- src/style/global.ts
+++ src/style/global.ts
@@ -1,12 +1,6 @@
 import styled, { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    overflow: hidden;
-  }
   html{
     overflow: hidden;
     margin: 0;
