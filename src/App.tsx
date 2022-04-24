5598acb66d00769835db4c66d860a7adcf9e6300
Anny Gabrielly <annyjp05@gmail.com>
2022-04-24 11:38:49 -0300

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react"
-import './style/style.css'
 import Main from "./page/Main"
 
 function App() {
 
   return (
-    <div className='app'>
+    <>
       <Main/>
-    </div>
+    </>
   )
 }
 
